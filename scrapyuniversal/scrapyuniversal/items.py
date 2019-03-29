578efe13e9301809acdf686511ab1484e56a9649
TAOJIANGCAI <826907729@qq.com>
2019-03-29 14:53:36 +0800

    scrapy 通用爬虫

---
+++ scrapyuniversal/scrapyuniversal/items.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, Compose, Join
+
+
+class ScrapyuniversalItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()
+    url = scrapy.Field()
+    text = scrapy.Field()
+    datetime = scrapy.Field()
+    source = scrapy.Field()
+    website = scrapy.Field()
+
+
+class NewsLoader(ItemLoader):
+    default_input_processor = TakeFirst()
+
+
+class ChinaLoader(NewsLoader):
+    text_out = Compose(Join(), lambda s: s.strip())
+    source_out = Compose(Join(), lambda s: s.strip())
---
+++ scrapyuniversal/scrapyuniversal/spiders/china.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from scrapyuniversal.items import ScrapyuniversalItem, ChinaLoader
+from scrapy.loader import ItemLoader
+
+
+class ChinaSpider(CrawlSpider):
+    name = 'china'
+    allowed_domains = ['tech.china.com']
+    start_urls = ['http://tech.china.com/articles/']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'article\/.*\.html', restrict_xpaths='//*[@id="left_side"]/div[@class="con_item"]'),
+             callback='parse_item'),
+        Rule(LinkExtractor(restrict_xpaths='//div[@id="pageStyle"]//a[contains(., "下一页")]'))
+
+    )
+
+    def parse_item(self, response):
+        loader = ChinaLoader(item=ScrapyuniversalItem(), response=response)
+        # item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
+        # item['name'] = response.xpath('//div[@id="name"]').get()
+        # item['description'] = response.xpath('//div[@id="description"]').get()
+        loader.add_xpath('title', '//h1[@id="chan_newsTitle"]/text()')
+        loader.add_value('url', response.url)
+        loader.add_xpath('text', '//div[@id="chan_newsDetail"]//text()')
+        loader.add_xpath('datetime', '//div[@id="chan_newsInfo"]/text()', re='(\d+-\d+-\d+\s\d+:\d+:\d+)')
+        loader.add_xpath('source', '//div[@id="chan_newsInfo"]/text()', re='来源：(.*)')
+        loader.add_value('website', '中华网')
+        yield loader.load_item()
